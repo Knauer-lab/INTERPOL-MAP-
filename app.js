16bcd17e5006b3c4832badd367817c2ee690d4d0
Knauer-lab <s.schomburg@gmx.de>
2026-07-07 21:00:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,5 +7,46 @@ L.tileLayer(
   }
 ).addTo(map);
 
-document.getElementById('info').innerHTML =
-  '<h3>✅ JavaScript funktioniert</h3><p>Die Verbindung zwischen HTML und app.js ist erfolgreich.</p>';
+Promise.all([
+  fetch('countries.json').then(r => r.json()),
+  fetch('world.geojson').then(r => r.json())
+]).then(([countryData, worldData]) => {
+
+  L.geoJSON(worldData, {
+
+    style: {
+      color: '#333',
+      weight: 1,
+      fillColor: '#4a90e2',
+      fillOpacity: 0.5
+    },
+
+    onEachFeature: function(feature, layer) {
+
+      layer.on('click', function() {
+
+        const country = feature.properties.name;
+
+        const info = countryData[country];
+
+        if (!info) {
+
+          document.getElementById('info').innerHTML =
+            `<h3>${country}</h3><p>Keine Daten vorhanden.</p>`;
+
+          return;
+        }
+
+        document.getElementById('info').innerHTML = `
+          <h3>${country}</h3>
+          <p><b>NCB:</b> ${info.ncb}</p>
+          <p><b>Red Notice:</b> ${info.redNotice}</p>
+          <p><b>Diffusion:</b> ${info.diffusion}</p>
+        `;
+      });
+
+    }
+
+  }).addTo(map);
+
+});
