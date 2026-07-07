9950464a1744c79e3bcfd6b213f0d796f710299c
Knauer-lab <s.schomburg@gmx.de>
2026-07-07 20:49:56 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const map = L.map('map').setView([20,0],2);
+
+L.tileLayer(
+  'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; OpenStreetMap'
+  }
+).addTo(map);
+
+document.getElementById('info').innerHTML =
+  'Die Karte wurde erfolgreich geladen.';
